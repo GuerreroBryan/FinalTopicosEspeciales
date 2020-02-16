36493e05ef0bf3d01138708170225cb3a2168bef
GuerreroBryan <bryan.guerrero01@epn.edu.ec>
2020-02-16 12:45:10 -0500

    API

---
+++ src/app/model/iPelis.interface.ts
@@ -0,0 +1,11 @@
+export interface iPelis {
+    imdbId: string;
+    Title: string;
+    Genre?: string;
+    ImdbRating: string;
+    Year: string;
+    Director: string;
+    Actors: string;
+    Poster: string;
+    Type: string;
+}
---
+++ src/app/pages/pelis/pelis.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { PeliService } from '../../services/peli.service';
+import { iPelis } from '../../model/iPelis.interface';
+
+
+@Component({
+  selector: 'app-pelis',
+  templateUrl: './pelis.page.html',
+  styleUrls: ['./pelis.page.scss'],
+})
+export class PelisPage implements OnInit {
+
+  results : Observable<iPelis>;
+  term: string='';
+  type: string='';
+
+  constructor( private peliService: PeliService) { }
+
+  ngOnInit() {
+  }
+
+
+  searchChanged(): void{
+    this.results = this.peliService.searchMovies(this.term, this.type);
+  }
+
+}

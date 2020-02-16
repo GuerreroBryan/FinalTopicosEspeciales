94322d46ba0b800fd863505d3fb1cab6edd6f49b
GuerreroBryan <bryan.guerrero01@epn.edu.ec>
2020-02-16 16:14:43 -0500

    Correccion Visual

--- src/app/pages/pelis/pelis.page.ts
+++ src/app/pages/pelis/pelis.page.ts
@@ -4,6 +4,10 @@ import { Observable } from 'rxjs';
 import { PeliService } from '../../services/peli.service';
 import { iPelis } from '../../model/iPelis.interface';
 
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-pelis',
@@ -16,7 +20,8 @@ export class PelisPage implements OnInit {
   term: string='';
   type: string='';
 
-  constructor( private peliService: PeliService) { }
+  constructor( private peliService: PeliService,  
+    private authService: AuthService, private router: Router, private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
   }
@@ -24,6 +29,12 @@ export class PelisPage implements OnInit {
 
   searchChanged(): void{
     this.results = this.peliService.searchMovies(this.term, this.type);
+  }
+
+  onLogout(){
+    console.log('Hasta la proxima!');
+    this.afAuth.auth.signOut();
+    this.router.navigateByUrl('/register');
   }
 
 }
